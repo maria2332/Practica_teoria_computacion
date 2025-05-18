9612c63a7330d8fdabe3448c7ff08134f7ffc6a0
maria2332 <maria.ballesteros2004@gmail.com>
2025-05-18 22:35:58 +0200

    lexer

--- lexer.py
+++ lexer.py
@@ -1,40 +1,28 @@
 import ply.lex as lex
 
-tokens = [
-    'TAG_OPEN', 'TAG_CLOSE', 'TAG_SLASH_CLOSE',
-    'A_OPEN', 'IMG_OPEN',
-    'HREF_ATTR', 'SRC_ATTR',
-    'URL'
-]
+# Lista de tokens que vamos a usar
+tokens = ['HREF', 'SRC', 'URL']
 
-t_TAG_OPEN = r'<'
-t_TAG_CLOSE = r'>'
-t_TAG_SLASH_CLOSE = r'/>'
-
-def t_A_OPEN(t):
-    r'[aA]'
-    return t
-
-def t_IMG_OPEN(t):
-    r'[iI][mM][gG]'
-    return t
-
-def t_HREF_ATTR(t):
+# Reglas simples
+def t_HREF(t):
     r'href'
     return t
 
-def t_SRC_ATTR(t):
+def t_SRC(t):
     r'src'
     return t
 
 def t_URL(t):
     r'\"(http[s]?://[^"]+)\"'
-    t.value = t.value.strip('\"')
+    t.value = t.value.strip('"')
     return t
 
+# Ignorar espacios, tabs y saltos de línea
 t_ignore = ' \t\n'
 
+# Manejo de errores
 def t_error(t):
     t.lexer.skip(1)
 
+# Construir el lexer
 lexer = lex.lex()
