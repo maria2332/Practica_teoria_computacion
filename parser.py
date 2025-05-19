7d0482407686096bbd1efa6cc079aceec6e8fdaf
maria2332 <maria.ballesteros2004@gmail.com>
2025-05-19 17:36:04 +0200

    cambio

--- parser.py
+++ parser.py
@@ -18,30 +18,48 @@ def is_html_balanced(html):
             stack.pop()
     return len(stack) == 0
 
+
 def parse_html(html):
     hrefs = []
     srcs = []
+
     lexer.input(html)
-    context = None
-    expect_value = None
+    tag_context = None   # 'a' o 'img'
+    attr_target = None   # 'href' o 'src'
+    awaiting_equals = False
+    awaiting_value = False
 
     for tok in lexer:
         if tok.type == 'TAG_OPEN_A':
-            context = 'a'
+            tag_context = 'a'
         elif tok.type == 'TAG_OPEN_IMG':
-            context = 'img'
-        elif tok.type in ['TAG_CLOSE']:
-            context = None
-            expect_value = None
-        elif tok.type == 'HREF' and context == 'a':
-            expect_value = 'href'
-        elif tok.type == 'SRC' and context == 'img':
-            expect_value = 'src'
-        elif tok.type == 'URL' and expect_value == 'href':
-            hrefs.append(tok.value)
-            expect_value = None
-        elif tok.type == 'URL' and expect_value == 'src':
-            srcs.append(tok.value)
-            expect_value = None
+            tag_context = 'img'
+
+        elif tok.type == 'HREF' and tag_context == 'a':
+            attr_target = 'href'
+            awaiting_equals = True
+
+        elif tok.type == 'SRC' and tag_context == 'img':
+            attr_target = 'src'
+            awaiting_equals = True
+
+        elif tok.type == 'EQUALS' and awaiting_equals:
+            awaiting_equals = False
+            awaiting_value = True
+
+        elif tok.type == 'URL' and awaiting_value:
+            if attr_target == 'href':
+                hrefs.append(tok.value)
+            elif attr_target == 'src':
+                srcs.append(tok.value)
+            # reset after use
+            attr_target = None
+            awaiting_value = False
+
+        elif tok.type == 'TAG_CLOSE':
+            tag_context = None
+            attr_target = None
+            awaiting_equals = False
+            awaiting_value = False
 
     return hrefs, srcs
