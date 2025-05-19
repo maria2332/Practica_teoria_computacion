5627007f77b9225fa1cbe26d42c9f3ddd11a84fa
maria2332 <maria.ballesteros2004@gmail.com>
2025-05-19 21:37:23 +0200

    lexer final

--- lexer.py
+++ lexer.py
@@ -1,21 +1,21 @@
-import ply.lex as lex
+import ply.lex as lex # Lexer
 
-tokens = ('TAG_OPEN', 'TAG_CLOSE', 'TAG_SLASH_CLOSE', 'HREF', 'SRC')
+tokens = ('TAG_OPEN', 'TAG_CLOSE', 'TAG_SLASH_CLOSE', 'HREF', 'SRC') # Tokens
 
-t_TAG_OPEN = r'<[a-zA-Z]+'
-t_TAG_CLOSE = r'</[a-zA-Z]+>'
+t_TAG_OPEN = r'<[a-zA-Z]+' # Etiquetas de apertura
+t_TAG_CLOSE = r'</[a-zA-Z]+>' # Etiquetas de cierre
 t_TAG_SLASH_CLOSE = r'/>'  # autocierre
-t_ignore = ' \t\n'
+t_ignore = ' \t\n' # Ignorar espacios, tabulaciones y saltos de línea
 
-def t_HREF(t):
-    r'href\s*=\s*"[^"]+"'
-    return t
+def t_HREF(t): # Enlaces
+    r'href\s*=\s*"[^"]+"' # Expresión regular para href
+    return t # Devolver el token
 
-def t_SRC(t):
-    r'src\s*=\s*"[^"]+"'
-    return t
+def t_SRC(t): # Imágenes
+    r'src\s*=\s*"[^"]+"' # Expresión regular para src
+    return t # Devolver el token
 
-def t_error(t):
-    t.lexer.skip(1)
+def t_error(t): # Manejo de errores
+    t.lexer.skip(1) # Ignorar el token no reconocido
 
-lexer = lex.lex()
+lexer = lex.lex() # Crear el lexer
