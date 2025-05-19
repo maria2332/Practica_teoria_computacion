9c9e15b2ecb3f7e630cfeebc9a99cca9a2785475
maria2332 <maria.ballesteros2004@gmail.com>
2025-05-19 13:09:43 +0200

    parser

--- parser.py
+++ parser.py
@@ -1,28 +1,38 @@
-import re
+import ply.yacc as yacc
+from lexer import tokens
 
-# Lista de etiquetas que no necesitan cierre
-VOID_TAGS = {
-    'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img',
-    'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr'
-}
+stack = []
 
-def is_html_balanced(html):
-    # Encuentra etiquetas de apertura y cierre
-    tags = re.findall(r'<(/?)([a-zA-Z0-9]+)', html)
-    
-    stack = []
+def p_html(p):
+    '''html : elementos'''
+    if stack:
+        print("HTML mal balanceado. Pila final:", stack)
+    else:
+        print("HTML bien balanceado.")
 
-    for slash, tag in tags:
-        tag = tag.lower()
+def p_elementos(p):
+    '''elementos : elemento elementos
+                 | '''
+    pass
 
-        if tag in VOID_TAGS:
-            continue  # ignorar autoconclusivas
+def p_elemento_tag_open(p):
+    'elemento : TAG_OPEN'
+    tag = p[1].split()[0][1:]  # extrae nombre sin "<"
+    stack.append(tag)
 
-        if not slash:  # etiqueta de apertura
-            stack.append(tag)
-        else:  # etiqueta de cierre
-            if not stack or stack[-1] != tag:
-                return False
-            stack.pop()
+def p_elemento_tag_close(p):
+    'elemento : TAG_CLOSE'
+    tag = p[1][2:-1]
+    if not stack or stack[-1] != tag:
+        print(f"Error: cerrando {tag} sin corresponder.")
+    else:
+        stack.pop()
 
-    return len(stack) == 0
+def p_elemento_self_closing(p):
+    'elemento : SELF_CLOSING'
+    pass  # no necesita balanceo
+
+def p_error(p):
+    print("Error de sintaxis")
+
+parser = yacc.yacc()
