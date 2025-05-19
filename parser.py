0d99871aed5601983718f7cf4afeb26126a5eec4
maria2332 <maria.ballesteros2004@gmail.com>
2025-05-19 17:17:49 +0200

    parser

--- parser.py
+++ parser.py
@@ -1,23 +1,3 @@
-import re
-from lexer import lexer
-
-def is_html_balanced(html):
-    stack = []
-    tags = re.findall(r'<(/?)([a-zA-Z0-9]+)', html)
-    void_tags = {'br', 'img', 'meta', 'hr', 'input', 'link', 'source', 'track', 'area', 'base', 'col', 'embed', 'wbr'}
-
-    for slash, tag in tags:
-        tag = tag.lower()
-        if tag in void_tags:
-            continue
-        if not slash:
-            stack.append(tag)
-        else:
-            if not stack or stack[-1] != tag:
-                return False
-            stack.pop()
-    return len(stack) == 0
-
 def parse_html(html):
     hrefs = []
     srcs = []
@@ -28,14 +8,16 @@ def parse_html(html):
 
     for tok in lexer:
         if tok.type == 'TAG_NAME':
-            last_tag = tok.value
+            last_tag = tok.value.lower()
 
         elif last_token == 'HREF' and tok.type == 'URL' and last_tag == 'a':
             hrefs.append(tok.value)
-            last_tag = None
+
         elif last_token == 'SRC' and tok.type == 'URL' and last_tag == 'img':
             srcs.append(tok.value)
-            last_tag = None
+
+        elif tok.value in ['>', '/>']:
+            last_tag = None  # se cierra la etiqueta
 
         last_token = tok.type
 
